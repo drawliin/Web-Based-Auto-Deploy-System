892bcabb4961328b9e4ec8b5a73eb7c852d23125
drawlin <itshoussameddine@gmail.com>
2025-02-07 23:57:58 +0100

    Remove production flag from frontend build command in Dockerfile

--- nodejs/server.js
+++ nodejs/server.js
@@ -128,7 +128,7 @@ const createFrontendDockerfile = (repoPath, frontendTech) => {
       COPY package*.json ./
       RUN npm install
       COPY ./ ./
-      RUN npm run build --prod
+      RUN npm run build
 
       FROM nginx:alpine
       COPY --from=build /app/dist/frontend/browser /usr/share/nginx/html
